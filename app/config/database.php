c49f3f348a731caa0830ae2faaf0d2398bf18adc
Jaylord <mazojaylord@gmail.com>
2025-09-25 04:55:43 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -58,16 +58,16 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-    'driver'	=> 'mysql',
-    'hostname'	=> getenv('DB_HOST') ?: 'localhost',
-    'port'		=> getenv('DB_PORT') ?: '3306',
-    'username'	=> getenv('DB_USERNAME') ?: 'root',
-    'password'	=> getenv('DB_PASSWORD') ?: '',
-    'database'	=> getenv('DB_DATABASE') ?: 'database',
-    'charset'	=> 'utf8mb4',
+    'driver'	=> 'sqlite',
+    'hostname'	=> 'sql12.freesqldatabase.com',
+    'port'		=> '3306',
+    'username'	=> 'sql12799992',
+    'password'	=> 'vw9hrVIzqM',
+    'database'	=> 'sql12799992',
+    'charset'	=> 'utf8',
     'dbprefix'	=> '',
     // Optional for SQLite
-    'path'      => ''
+    'path'      => 'runtime/database.db'
 );
 
 ?>
